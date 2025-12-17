b9e228bff8b060786ed1fefb217cc6e4740aa3c9
prajapatisumit8988-del <prajapatisumit8988@gmail.com>
2025-12-16 20:10:42 -0800

    update

--- js/chat.js
+++ js/chat.js
@@ -1,57 +1,58 @@
 import { auth, database } from "./firebase.js";
-import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { ref, push, onChildAdded } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import { onAuthStateChanged } from
+"https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { ref, onValue, push, set } from
+"https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
-const chatBox = document.getElementById("chatBox");
-const messageInput = document.getElementById("message");
-
-let userEmail = "";
+let currentUser = "";
+let selectedUser = "";
 
 onAuthStateChanged(auth, user => {
-    if (!user) {
-        window.location.href = "index.html";
-    } else {
-        userEmail = user.email.replace(".", "_");
-        loadMessages();
-    }
+  if (!user) location.href = "index.html";
+  currentUser = user.email.replace(".", "_");
+  loadUsers();
 });
 
-window.sendMessage = function() {
-    const text = messageInput.value.trim();
-    if (text === "") return;
-
-    const messagesRef = ref(database, "messages");
-    push(messagesRef, {
-        user: userEmail,
-        message: text,
-        time: Date.now()
-    });
-
-    messageInput.value = "";
-}
-
-function loadMessages() {
-    const messagesRef = ref(database, "messages");
-    onChildAdded(messagesRef, snapshot => {
-        const data = snapshot.val();
-
+function loadUsers() {
+  onValue(ref(database, "users"), snap => {
+    userList.innerHTML = "";
+    snap.forEach(child => {
+      if (child.key !== currentUser) {
         const div = document.createElement("div");
-        div.classList.add("message");
-
-        if (data.user === userEmail) {
-            div.classList.add("sent");
-        } else {
-            div.classList.add("received");
-        }
-
-        div.innerHTML = `<strong>${data.user.replace("_", ".")}</strong><br>${data.message}`;
-        chatBox.appendChild(div);
-        chatBox.scrollTop = chatBox.scrollHeight;
+        div.innerText = child.val().email;
+        div.onclick = () => openChat(child.key);
+        userList.appendChild(div);
+      }
     });
+  });
 }
 
-window.logout = function() {
-    signOut(auth).then(() => {
-        window.location.href = "index.html";
+function openChat(userId) {
+  selectedUser = userId;
+  chatBox.innerHTML = "";
+
+  const chatId = [currentUser, selectedUser].sort().join("__");
+
+  onValue(ref(database, "chats/" + chatId), snap => {
+    chatBox.innerHTML = "";
+    snap.forEach(msg => {
+      const p = document.createElement("p");
+      p.innerText = msg.val().text;
+      chatBox.appendChild(p);
     });
+  });
 }
+
+window.sendMessage = function () {
+  if (!selectedUser) return alert("Select user first");
+
+  const chatId = [currentUser, selectedUser].sort().join("__");
+
+  push(ref(database, "chats/" + chatId), {
+    from: currentUser,
+    text: message.value,
+    time: Date.now()
+  });
+
+  message.value = "";
+};
--- js/script.js
+++ js/script.js
@@ -1,27 +1,26 @@
-import { auth } from "./firebase.js";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { auth, database } from "./firebase.js";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from
+"https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { ref, set } from
+"https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
-const msg = document.getElementById("msg");
+window.signup = function () {
+  const email = email.value;
+  const password = password.value;
 
-window.signup = function() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      const uid = email.replace(".", "_");
+      set(ref(database, "users/" + uid), {
+        email: email
+      });
+      location.href = "chat.html";
+    })
+    .catch(err => alert(err.message));
+};
 
-    if(!email || !password) {
-        msg.innerText = "Email aur Password bharo!";
-        return;
-    }
-
-    createUserWithEmailAndPassword(auth, email, password)
-    .then(() => msg.innerText = "Signup successful! Login karo.")
-    .catch(err => msg.innerText = err.message);
-}
-
-window.login = function() {
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-
-    signInWithEmailAndPassword(auth, email, password)
-    .then(() => window.location.href = "chat.html")
-    .catch(err => msg.innerText = err.message);
-}
+window.login = function () {
+  signInWithEmailAndPassword(auth, email.value, password.value)
+    .then(() => location.href = "chat.html")
+    .catch(err => alert(err.message));
+};
