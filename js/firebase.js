38309a61eba29189a9f1e440dc261edd20e69905
prajapatisumit8988-del <prajapatisumit8988@gmail.com>
2025-12-16 20:27:40 -0800

    update

--- js/firebase.js
+++ js/firebase.js
@@ -1,18 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+import { initializeApp } from
+"https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getAuth, RecaptchaVerifier } from
+"https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+import { getDatabase } from
+"https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDX2K-8zFXnniHKBHXq1L9EURF727QtLjY",
   authDomain: "talkzing-4364d.firebaseapp.com",
   databaseURL: "https://talkzing-4364d-default-rtdb.firebaseio.com",
   projectId: "talkzing-4364d",
-  storageBucket: "talkzing-4364d.appspot.com",
-  messagingSenderId: "880336069904",
-  appId: "1:880336069904:web:e561d53cab028b8e857195"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const database = getDatabase(app);
+export { RecaptchaVerifier };
--- js/script.js
+++ js/script.js
@@ -1,26 +1,38 @@
-import { auth, database } from "./firebase.js";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from
+import { auth, database, RecaptchaVerifier } from "./firebase.js";
+import { signInWithPhoneNumber } from
 "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 import { ref, set } from
 "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
 
-window.signup = function () {
-  const email = email.value;
-  const password = password.value;
+window.recaptchaVerifier = new RecaptchaVerifier(
+  'recaptcha',
+  { size: 'normal' },
+  auth
+);
 
-  createUserWithEmailAndPassword(auth, email, password)
-    .then(() => {
-      const uid = email.replace(".", "_");
-      set(ref(database, "users/" + uid), {
-        email: email
-      });
-      location.href = "chat.html";
+let confirmationResult;
+
+window.sendOTP = function () {
+  const phone = phone.value;
+  signInWithPhoneNumber(auth, phone, window.recaptchaVerifier)
+    .then(result => {
+      confirmationResult = result;
+      msg.innerText = "OTP sent!";
     })
     .catch(err => alert(err.message));
 };
 
-window.login = function () {
-  signInWithEmailAndPassword(auth, email.value, password.value)
-    .then(() => location.href = "chat.html")
-    .catch(err => alert(err.message));
+window.verifyOTP = function () {
+  const code = otp.value;
+  confirmationResult.confirm(code).then(result => {
+    const user = result.user;
+    const uid = user.uid;
+
+    set(ref(database, "users/" + uid), {
+      name: name.value,
+      phone: user.phoneNumber
+    });
+
+    location.href = "chat.html";
+  });
 };
